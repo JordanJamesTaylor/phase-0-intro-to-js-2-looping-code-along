340e7079e884b8445ea682d5288aabcc451f78d1
jordan <jordjamestaylor@gmail.com>
2022-05-15 11:20:18 -0400

    commit tree 0f5d1d05c2084d2814090c9a178cb84c63dbc936

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`)
+  };
+
+  return gifts;
+};
+
+function writeCards(arrayNames, eventName){
+    const messages = [];
+
+    for (let i = 0; i < arrayNames.length; i++){
+        messages.push(`Thank you, ${arrayNames[i]}, for the wonderful ${eventName} gift!`)
+        console.log(messages[i])
+        debugger;
+    };
+
+    return messages;
+};
+
+//wrapGifts(gifts);
+//writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+
+        num--;
+    };
+};
